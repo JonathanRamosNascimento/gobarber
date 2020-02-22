27af7cc90cd3c56c9c3689f96f7ac6c488597248
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-02-22 19:09:02 -0300

    Criando o loader dos models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Joathan Ramos',
+    email: 'ramos.was.jonathan@gmail.com',
+    password_hash: '56454313843',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
