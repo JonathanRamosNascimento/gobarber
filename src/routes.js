17d28d77c1887594dad597d1725b1e2115f4fc0a
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-02-23 10:28:58 -0300

    Criando o cadastro de usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Joathan Ramos',
-    email: 'ramos.was.jonathan@gmail.com',
-    password_hash: '56454313843',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
