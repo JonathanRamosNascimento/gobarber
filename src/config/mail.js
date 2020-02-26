e43f22f310d7333660e72526cbde7285ab822fe3
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-02-25 21:24:16 -0300

    Configurando o nodemailer com o Mailtrap(dev) para enviar um email para o prestador do serviço avisando quando um agendamento foi cancelado

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '4e520514216ea0',
+    pass: '7a3269c4e1276d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
